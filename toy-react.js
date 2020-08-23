a07f2a11151d9f3b770071113678fadebb4cac33
ewl <amoryvictorialee@outlook.com>
2020-08-23 20:19:18 +0800

    完善代码

--- toy-react.js
+++ toy-react.js
@@ -1,50 +1,12 @@
 const RENDER_TO_DOM = Symbol('render to dom');
-class ElementWrapper {
-    // 因为渲染的时候，我们使用的是new type，所以必然class是一个可以使用new来操作的构造函数
-    constructor(type) {
-        // 这里的type是用于传递createElement的标签名的，我们在创建一个组件的时候，必然需要传递一个元素标签进去
-        this.root = document.createElement(type);
-    }
 
-    // 要实现setAttribute和appendChild
-    setAttribute(name, value) {
-        // \s\S这样的正则表达式是一种比较稳妥的表示全部字符的方式
-        if (name.match(/^on([\s\S]+)/)) {
-            // 如果我们写的是onClick这样的格式，需要将驼峰的写法改成小写事件名称
-            this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value);
-        } else {
-            if (name === 'className') {
-                this.root.setAttribute('class', value);
-            } else {
-                this.root.setAttribute(name, value);
-            }
-        }
-    }
-    appendChild(component) {
-        let range = document.createRange();
-        range.setStart(this.root, this.root.childNodes.length); // offset设置为0 表示从parentElement的第一个children到最后一个children
-        // 这里之所以使用childNodes而不使用children是因为我们需要获取到所有的节点，包括注释以及文本在内，
-        // 而children只会获取到元素节点
-        range.setEnd(this.root, this.root.childNodes.length);
-        component[RENDER_TO_DOM](range);
-    }
-    [RENDER_TO_DOM](range) {
-        range.deleteContents();
-        range.insertNode(this.root);
-    }
-}
+function replaceContent(range, node) {
+    range.insertNode(node);
+    range.setStartAfter(node);
+    range.deleteContents();
 
-// 文本节点没有属性，所以不需要设置setAttribute
-// 当然文本节点自己也没有子节点，自然是不需要appendChild
-class TextWrapper {
-    constructor(content) {
-        // 这里的content是用于给createTextNode做为文案参数的，所以必然是一个字符串
-        this.root = document.createTextNode(content);
-    }
-    [RENDER_TO_DOM](range) {
-        range.deleteContents();
-        range.insertNode(this.root);
-    }
+    range.setStartBefore(node);
+    range.setEndAfter(node);
 }
 
 export class Component {
@@ -52,6 +14,7 @@ export class Component {
         this.props = Object.create(null);
         this.children = [];
         this._range = null;
+
     }
 
     setAttribute(name, value) {
@@ -62,29 +25,104 @@ export class Component {
         this.children.push(component);
     }
 
+    get vdom() {
+        // 因为Component的内容是由render决定的，所以直接执行render返回对应的组件内容
+        // 这里的this.render仍然是一个递归的调用，如果this.render出来的结果是一个Component，那么仍旧会继续去调用这个内部的vdom getter
+        // 直到最后获取到普通的元素节点或者文本节点
+        return this.render().vdom;
+    }
+
     // 私有方法
     [RENDER_TO_DOM](range) {
         this._range = range;
-        this.render()[RENDER_TO_DOM](range);
+        this._vdom = this.vdom;
+        // 这里原来的写法是this.root, 但是实际上这里的更新并不是在做真正的DOM更新
+        // 所以不能直接使用真实DOM进行处理，要使用虚拟DOM进行，而虚拟DOM则需要将当时的vdom进行存储，
+        // 即上面的this._vdom
+        // this.root[RENDER_TO_DOM](range);
+        this._vdom[RENDER_TO_DOM](range);
     }
 
-    // 重新绘制
-    rerender() {
-        let oldRange = this._range;
-        // 先进行range的插入，再将旧的range清除，这样就可以保证tic-tac-toe里面不会出现空格丢失的情况了
-        // 这里出现问题的原因在于，如果我们的range内容空掉了，那么这个range实际上是会消失的，这个range后面跟着的
-        // range会紧接着补上来导致旧range被吃掉，为了不让旧range被吃掉，我们需要在旧range的内容前面加入一个新的range
-        let range = document.createRange();
-        range.setStart(oldRange.startContainer, oldRange.startOffset);
-        range.setEnd(oldRange.startContainer, oldRange.startOffset);
-        // 调用renderToDom方法去渲染
-        this[RENDER_TO_DOM](range);
+    update() {
+        let isSameNode = (oldNode, newNode) => {
+            // 类型不同
+            if (oldNode.type !== newNode.type) {
+                return false;
+            }
 
-        // 这里要注意，如果要删除旧range，必须要把旧range的范围进行重设，这里需要把旧range的开始位置挪到新range的结束位置
-        // 因为在插入新range的时候必然会导致旧range的扩大
-        oldRange.setStart(range.endContainer, range.endOffset);
-        oldRange.deleteContents();
+            // 属性不同
+            // for-in适合遍历对象，因为for-in主要面向的是遍历对象的属性，如果恰好这个对象是个数组的话，那么遍历的就是数组的索引
+            for (let name in newNode.props) {
+                if (newNode.props[name] !== oldNode.props[name]) {
+                    return false;
+                }
+            }
+
+            // todo 留一个疑问，这里为什么是>而不是!==
+            if (Object.keys(oldNode.props).length > Object.keys(newNode.props).length) {
+                return false;
+            }
+
+            if (newNode.type === '#text') {
+                if (newNode.content !== oldNode.content) {
+                    return false;
+                }
+            }
+
+            return true;
+        };
+
+
+        let update = (oldNode, newNode) => {
+            // 节点对比的顺序
+            // type props children 如果type或者props不同则说明是完全不一样的子节点 除非根节点的type和props是完全一致的，我们
+            // 才认为这个根节点是不需要更新的，之后才会去看根节点里面的children
+            // 如果是文本节点，那么需要去关注它的content
+
+            // 进行旧结点的覆盖
+            if (!isSameNode(oldNode, newNode)) {
+                // 这里是一个完全的全新渲染
+                newNode[RENDER_TO_DOM](oldNode._range);
+                return;
+            }
+
+            newNode._range = oldNode._range;
+
+            let newChildren = newNode.vchildren;
+            let oldChildren = oldNode.vchildren;
+
+            if (!newChildren || !newChildren.length) {
+                return;
+            }
+
+            let tailRange = oldChildren[oldChildren.length - 1]._range;
+
+            for (let i = 0; i < newChildren.length; i++) {
+                let newChild = newChildren[i];
+                let oldChild = oldChildren[i];
+
+                if (i < oldChildren.length) {
+                    update(oldChild, newChild);
+                } else {
+                    // 如果newChildren的长度比newChildren的长，就说明新的子节点是要多于旧节点
+                    // 首先获取一下旧的节点的最后一个边缘位置，在这里进行新节点的插入
+                    let range = document.createRange();
+                    range.setStart(tailRange.endContainer, tailRange.endOffset);
+                    range.setEnd(tailRange.endContainer, tailRange.endOffset);
+                    newChild[RENDER_TO_DOM](range);
+
+                    // 后面如果继续要追加range，就必须让tailRange往后稍稍
+                    tailRange = range;
+                }
+            }
+        };
+
+        let vdom = this.vdom;
+        update(this._vdom, vdom);
+
+        this._vdom = vdom; // 将新的vdom替换到当前的vdom上来
     }
+
 
     // 假设此时已经有state对象了，考虑React中state的表现，这里的state需要做一个属性的深拷贝合并
     // 比如一个结构稍微复杂的state对象，在更新某一个属性的时候，是将旧的state对象拷贝到新的state对象上
@@ -93,7 +131,7 @@ export class Component {
     setState(newState) {
         if (newState === null || typeof newState !== 'object') {
             this.state = newState;
-            this.rerender();
+            this.update();
             return;
         }
         // merge会被递归地调用
@@ -109,7 +147,91 @@ export class Component {
         };
 
         merge(this.state, newState);
-        this.rerender();
+        this.update();
+    }
+}
+
+class ElementWrapper extends Component {
+    // 因为渲染的时候，我们使用的是new type，所以必然class是一个可以使用new来操作的构造函数
+    constructor(type) {
+        // 以下是Component组件的构造函数里的内容, 在ElementWrapper里面使用super就可以调用父组件的构造函数
+        // 这让ElementWrapper也拥有了自己的props以及children
+        // this.props = Object.create(null);
+        // this.children = [];
+        // this._range = null;
+        super(type);
+        this.type = type;
+    }
+
+    // 获取虚拟DOM
+    get vdom () {
+        this.vchildren = this.children.map(child => child.vdom);
+        return this;
+        // return {
+            // 虚拟DOM中需要包含的三样东西
+            // type: this.type,
+            // props: this.props,
+            // children: this.children.map(child => child.vdom),
+        // };
+    }
+
+    [RENDER_TO_DOM](range) {
+        this._range = range;
+        let root = document.createElement(this.type);
+
+        // 一个虚拟DOM上最主要的内容就是三大块： root/props/children
+        for (let name in this.props) {
+            const value = this.props[name];
+            if (name.match(/^on([\s\S]+)/)) {
+                // 如果我们写的是onClick这样的格式，需要将驼峰的写法改成小写事件名称
+                root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value);
+            } else {
+                if (name === 'className') {
+                    root.setAttribute('class', value);
+                } else {
+                    root.setAttribute(name, value);
+                }
+            }
+        }
+
+        // 如果一上来就进行渲染会出现vchildren不存在的情况，所以为了保证可以获取到，这里必须进行判断
+        if (!this.vchildren) {
+            this.vchildren = this.children.map(child => child.vdom);
+        }
+        // children是一个数组，所以这里使用for-of
+        // 这里的处理实际上就是将children节点插入
+        for (let child of this.vchildren) {
+            let childRange = document.createRange();
+            childRange.setStart(root, root.childNodes.length); // offset设置为0 表示从parentElement的第一个children到最后一个children
+            // 这里之所以使用childNodes而不使用children是因为我们需要获取到所有的节点，包括注释以及文本在内，
+            // 而children只会获取到元素节点
+            childRange.setEnd(root, root.childNodes.length);
+
+            child[RENDER_TO_DOM](childRange);
+        }
+
+        replaceContent(range, root);
+    }
+}
+
+// 文本节点没有属性，所以不需要设置setAttribute
+// 当然文本节点自己也没有子节点，自然是不需要appendChild
+class TextWrapper extends Component {
+    constructor(content) {
+        super(content);
+        this.type = '#text';
+        this.content = content;
+        // 这里的content是用于给createTextNode做为文案参数的，所以必然是一个字符串
+    }
+
+    get vdom () {
+        return this;
+       }
+
+    [RENDER_TO_DOM](range) {
+        this._range = range;
+        let root = document.createTextNode(this.content);
+        replaceContent(range, root);
     }
 }
 
